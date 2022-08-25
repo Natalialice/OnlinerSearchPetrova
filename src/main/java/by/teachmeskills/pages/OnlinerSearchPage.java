3c31d09aa6c7d9514d9fcd248bdb7f65da765fb1
Natali Petrova <natalithewolf@gmail.com>
2022-08-25 17:12:32 +0300

    Initial commit

---
+++ src/main/java/by/teachmeskills/pages/OnlinerSearchPage.java
@@ -0,0 +1,34 @@
+package by.teachmeskills.pages;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.WebDriverRunner;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class OnlinerSearchPage {
+
+    public void searchInCatalog(String searchText) {
+        $(".fast-search__input").sendKeys(searchText);
+    }
+
+    public ElementsCollection getSearchResultProductTitles() {
+        switchToSearchFrame();
+        return $$(".result__item_product .product__title .product__title-link");
+    }
+
+    public ElementsCollection getSearchResultProductDetails() {
+        switchToSearchFrame();
+        return $$(".product__details .product__description");
+    }
+
+    private void switchToSearchFrame() {
+        if (!isInSearchFrame()) {
+            switchTo().frame($(".modal-iframe"));
+        }
+    }
+
+    public boolean isInSearchFrame() {
+        String frameUrl = WebDriverRunner.currentFrameUrl();
+        return frameUrl.contains("search/iframe");
+    }
+}
